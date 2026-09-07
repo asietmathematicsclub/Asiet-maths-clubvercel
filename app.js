0587c5a4494392edd3b6038556daecb10b34083e
asietmathematicsclub <asietmathematicsclub1234@gmail.com>
2026-09-07 19:33:27 +0530

    Update app.js
    Chatura: Battle of the Grid

--- app.js
+++ app.js
@@ -152,6 +152,16 @@ const STATIC_EVENTS = [];
        //link: "" // Replace with your Google Form URL
     //}
 
+{
+    id: 1,
+    title: "CHATURA: COMPETITIVE SUDOKU CHALLENGE",
+    date: "9 September, 2026",
+    time: "2 PM",
+    location: "Main Seminar Hall",
+    description: "“Success is the sum of small efforts, repeated day in and day out.” — Robert Collier. Mathematics Club ASIET presents CHATURA, a team Sudoku challenge that puts logic, precision, and teamwork to the test. Team Size: 2 Participants per Team | Rounds: 5 | Prize Pool: ₹2000. #chathura #mathematicsclubasiet #bshdepartment",
+    image_path: "uploads/Chatura.jpeg",
+    link: "https://forms.gle/XyAPfnRsKePrAsBp8"
+}
 
 const STATIC_EXECOM = [
     {
